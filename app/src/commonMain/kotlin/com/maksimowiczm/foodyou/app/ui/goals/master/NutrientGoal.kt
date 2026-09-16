c520005fe6d42f799bc9e7c4de075dd9aa98a572
Andrey Mivrenik <andrey.m@fastmail.com>
2026-09-16 14:48:58 +0300

    fix: visual overflow bug in the detailed goals screen

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/ui/goals/master/NutrientGoal.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/ui/goals/master/NutrientGoal.kt
@@ -3,15 +3,18 @@ package com.maksimowiczm.foodyou.app.ui.goals.master
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
@@ -94,8 +97,8 @@ internal fun NutrientGoal(
         )
 
     Column(modifier = modifier, verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            label()
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Box(modifier = Modifier.weight(1f).padding(end = 8.dp)) { label() }
             value()
         }
         LinearProgressIndicator(
