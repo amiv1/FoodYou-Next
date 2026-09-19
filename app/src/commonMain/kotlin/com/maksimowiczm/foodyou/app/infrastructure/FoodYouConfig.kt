d10a128f58e612cf56a1e12dc677381ae637e5f3
Andrey Mivrenik <andrey.m@fastmail.com>
2026-09-19 09:37:36 +0300

    feat: change privacy policy URI

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/infrastructure/FoodYouConfig.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/infrastructure/FoodYouConfig.kt
@@ -9,7 +9,7 @@ internal class FoodYouConfig : AppConfig, NetworkConfig {
     override val translationUri: String = "https://crowdin.com/project/food-you"
     override val sourceCodeUri: String = "https://github.com/amiv1/FoodYou-Next"
     override val issueTrackerUri: String = "https://github.com/amiv1/FoodYou-Next/issues"
-    override val privacyPolicyUri: String = "https://foodyou.maksimowiczm.com/privacy-policy"
+    override val privacyPolicyUri: String = "https://amiv1.github.io/foodtracker/privacy.html"
     override val openFoodFactsTermsOfUseUri: String = "https://world.openfoodfacts.org/terms-of-use"
     override val openFoodFactsPrivacyPolicyUri: String = "https://world.openfoodfacts.org/privacy"
     override val foodDataCentralPrivacyPolicyUri: String = "https://www.usda.gov/privacy-policy"
